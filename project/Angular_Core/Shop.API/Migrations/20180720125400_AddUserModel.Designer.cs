f013cfc04f55f3ce8d2fa6d7ee6d207c92f6010e
Marthaler, Mike <mike.marthaler@vitodata.ch>
2018-07-20 16:05:36 +0200

    Added User Model and AuthRepository User can register with validation

--- project/Angular_Core/Shop.API/Migrations/20180720125400_AddUserModel.Designer.cs
+++ project/Angular_Core/Shop.API/Migrations/20180720125400_AddUserModel.Designer.cs
@@ -10,9 +10,10 @@ using System;
 namespace Shop.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20180720125400_AddUserModel")]
+    partial class AddUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
