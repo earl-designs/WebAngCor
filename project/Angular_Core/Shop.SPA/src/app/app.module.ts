a466f19f734102a74a65d23555597f992419ae82
Mike Marthaler <earl-designs@hotmail.ch>
2018-07-21 22:23:24 +0200

    added some angular login / sign up stuff

--- project/Angular_Core/Shop.SPA/src/app/app.module.ts
+++ project/Angular_Core/Shop.SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,18 +6,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavTopComponent } from './nav-top/nav-top.component';
+import { AuthService } from './_services/auth.service';
+import { SignInUpComponent } from './sign-in-up/sign-in-up.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavTopComponent,
+      SignInUpComponent,
+      HomeComponent
    ],
    imports: [
       BrowserModule,
-      HttpModule
+      HttpModule,
+      FormsModule
    ],
-   providers: [],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
---
+++ project/Angular_Core/Shop.SPA/src/app/nav-top/nav-top.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav-top',
+  templateUrl: './nav-top.component.html',
+  styleUrls: ['./nav-top.component.css']
+})
+export class NavTopComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(data => {
+      console.log('logged in successfully');
+    }, error => {
+      console.log('failed to loggin');
+    });
+  }
+
+  logout() {
+    this.authService.userToken = null;
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+}
